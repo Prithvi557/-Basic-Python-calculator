494bd54d6d17424efb9d1cdf49aabf5e7e7d692b
Prithvi557 <prithvitomar13@gmail.com>
2025-06-29 11:34:49 +0530

    Add files via upload

---
+++ simple program.py
@@ -0,0 +1,11 @@
+a = int(input("enter the first number "))
+b = int(input("enter the second number "))
+c = a+b
+d  = abs(a-b)
+e = a*b
+f = a/b
+print("Addition:",c)
+print("Subtraction:",d)
+print("Multiplication:",e)
+print("Division:",f)
+
